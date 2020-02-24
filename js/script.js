fbd689bf704eac4d4e9eb28a9f1d52fc3b3580e7
alchepak <alchepak@gmail.com>
2020-02-24 17:41:03 +0800

    21. Переделана логика + Открытие всех popup'ов

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
-var closeButtons = document.querySelectorAll(".popup-close-button");
-var feedbackLink = document.querySelector(".feedback-open");
-
-var feedbackPopup = document.querySelector(".feedback-popup");
-var feedbackForm = feedbackPopup.querySelector("form");
-var feedbackInputs = feedbackForm.querySelectorAll("[name]");
-var feedbackName = feedbackPopup.querySelector("[name='userName']");
-var feedbackMail = feedbackPopup.querySelector("[name='userMail']");
-var feedbackMessage = feedbackPopup.querySelector("[name='userMessage']");
+/* local storage var */
 
 var isStorageSupport = true;
 var storageName = "";
 var storageMail = "";
 
-/* popup common */
+var closeButtons = document.querySelectorAll(".popup-close-button");
 
-for (var i = 0; i < closeButtons.length; i++) {
-  closeButtons[i].addEventListener("click", function() {
-    if (this.parentElement) {
-      this.parentElement.hidden = true;
-    }
-  });
+if (closeButtons) {
+  for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener("click", function() {
+      if (this.parentElement) {
+        this.parentElement.hidden = true;
+      }
+    });
+  }
 }
 
 window.addEventListener("keydown", function(evt) {
@@ -44,45 +38,118 @@ catch (err) {
   isStorageSupport = false;
 }
 
-/* feedback popup */
+/* map popup */
 
-// отменяем html-валидацию
-for (var i = 0; i < feedbackInputs.length; i++) {
-  feedbackInputs[i].required = false;
-}
+var mapLink = document.querySelector(".map");
+var mapPopup = document.querySelector(".map-popup");
 
-feedbackLink.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  feedbackPopup.hidden = false;
-  feedbackName.focus();
+if (mapLink) {
 
-  if (storageName) {
-    feedbackName.value = storageName;
-    feedbackMail.focus();
-  }
-
-  if (storageMail) {
-    feedbackMail.value = storageMail;
-    feedbackMessage.focus();
-  }
-});
-
-feedbackForm.addEventListener("submit", function(evt) {
-  feedbackPopup.classList.remove("invalid-data");
-
-  if (!feedbackName.value || !feedbackMail.value || !feedbackMessage.value) {
+  mapLink.addEventListener("click", function(evt) {
     evt.preventDefault();
 
-    // перезапуск css анимации
-    void feedbackPopup.offsetWidth;
-    feedbackPopup.classList.add("invalid-data");
-  }
-  else {
-    if (isStorageSupport) {
-      localStorage.setItem("technomartUserName", feedbackName.value);
-      localStorage.setItem("technomartUserMail", feedbackMail.value);
+    if (mapPopup) {
+      mapPopup.hidden = false;
     }
+  });
+
+}
+
+/* feedback popup */
+
+var feedbackPopup = document.querySelector(".feedback-popup");
+
+if (feedbackPopup) {
+
+  var feedbackLink = document.querySelector(".feedback-open");
+  var feedbackForm = feedbackPopup.querySelector("form");
+  var feedbackInputs = feedbackForm.querySelectorAll("[name]");
+  var feedbackName = feedbackPopup.querySelector("[name='userName']");
+  var feedbackMail = feedbackPopup.querySelector("[name='userMail']");
+  var feedbackMessage = feedbackPopup.querySelector("[name='userMessage']");
+
+  // отменяем html-валидацию
+  for (var i = 0; i < feedbackInputs.length; i++) {
+    feedbackInputs[i].required = false;
   }
-});
+
+  feedbackLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    feedbackPopup.hidden = false;
+    feedbackName.focus();
+
+    if (storageName) {
+      feedbackName.value = storageName;
+      feedbackMail.focus();
+    }
+
+    if (storageMail) {
+      feedbackMail.value = storageMail;
+      feedbackMessage.focus();
+    }
+  });
+
+  feedbackForm.addEventListener("submit", function(evt) {
+    feedbackPopup.classList.remove("invalid-data");
+
+    if (!feedbackName.value || !feedbackMail.value || !feedbackMessage.value) {
+      evt.preventDefault();
+
+      // перезапуск css анимации
+      void feedbackPopup.offsetWidth;
+      feedbackPopup.classList.add("invalid-data");
+    }
+    else {
+      if (isStorageSupport) {
+        localStorage.setItem("technomartUserName", feedbackName.value);
+        localStorage.setItem("technomartUserMail", feedbackMail.value);
+      }
+    }
+  });
+
+}
+
+/* catalog marks */
+
+var addMarkButtons = document.querySelectorAll(".button-add-mark");
+var linkMarks = document.querySelector(".link-marks");
+
+if (addMarkButtons) {
+
+  for (var i = 0; i < addMarkButtons.length; i++) {
+    addMarkButtons[i].addEventListener("click", function(evt) {
+      evt.preventDefault();
+
+      if (linkMarks) {
+        linkMarks.classList.add("item-added");
+      }
+    });
+  }
+
+}
+
+/* catalog products */
+
+var buyButtons = document.querySelectorAll(".button-buy");
+var productAddedPopup = document.querySelector(".product-added-popup");
+var linkCart = document.querySelector(".link-cart");
+
+if (buyButtons) {
+
+  for (var i = 0; i < buyButtons.length; i++) {
+    buyButtons[i].addEventListener("click", function(evt) {
+      evt.preventDefault();
+
+      if (productAddedPopup) {
+        productAddedPopup.hidden = false;
+      }
+
+      if (linkCart) {
+        linkCart.classList.add("item-added");
+      }
+    });
+  }
+
+}
 
 
