c3ea6ec10cd335112f954b8d5645e6bf57350b28
alchepak <alchepak@gmail.com>
2020-02-24 14:50:48 +0800

    21. Валидаци формы обратной связи + BugFix

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 var closeButtons = document.querySelectorAll(".popup-close-button");
 var feedbackLink = document.querySelector(".feedback-open");
+
 var feedbackPopup = document.querySelector(".feedback-popup");
+var feedbackForm = feedbackPopup.querySelector("form");
+var feedbackInputs = feedbackForm.querySelectorAll("[name]");
+var feedbackName = feedbackPopup.querySelector("[name='userName']");
+var feedbackMail = feedbackPopup.querySelector("[name='userMail']");
+var feedbackMessage = feedbackPopup.querySelector("[name='userMessage']");
 
 for (var i = 0; i < closeButtons.length; i++) {
   closeButtons[i].addEventListener("click", function() {
-    if (this.parentElement)
-    {
+    if (this.parentElement) {
       this.parentElement.hidden = true;
     }
   });
 }
 
+/* feedback popup */
+
+for (var i = 0; i < feedbackInputs.length; i++) {
+  // отменяем html-валидацию
+  feedbackInputs[i].required = false;
+}
+
 feedbackLink.addEventListener("click", function(evt) {
   evt.preventDefault();
   feedbackPopup.hidden = false;
+  feedbackName.focus();
+});
+
+feedbackForm.addEventListener("submit", function(evt) {
+  if (!feedbackName.value || !feedbackMail.value || !feedbackMessage.value) {
+    evt.preventDefault();
+
+
+  }
 });
